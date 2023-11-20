da3faefabd9dc66a8e64ae100dc3be0a181d0689
Vinicius elias <vinicius.sotc.9983@gmail.com>
2023-11-20 18:08:15 -0300

    Firts class: Position

--- src/Application/system.java
+++ src/Application/system.java
@@ -1,10 +1,14 @@
 package Application;
 
+import boardgame.Position;
+
 public class system {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello word!");
+		Position pos = new Position(3, 5);
+		
+		System.out.println(pos);
 	}
 
 }
