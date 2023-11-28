b163e99d585d0f7933aa4b80294bc28623107f97
Vinicius elias <vinicius.sotc.9983@gmail.com>
2023-11-28 13:30:32 -0300

    placing pieces on the board

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -9,16 +9,16 @@ public class UI {
 			for (int j=0; j<pieces.length; j++) {
 				printPiece(pieces[i][j]);
 			}
-			System.out.println(   );
+			System.out.println();
 		}
-		System.out.println("  a  b  c  d  e  f  g  h");
+		System.out.print("  a b c d e f g h ");
 	}
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
-			System.out.print("- ");
+			System.out.printf("-");
 		}
 		else {
-			System.out.println(piece);
+			System.out.print(piece);
 		}
 		System.out.print(" ");
 	}
